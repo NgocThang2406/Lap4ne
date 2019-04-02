a4fc4ae70f9ad818a9e33d4b3a17a931cc7f1dc9
Administrator <Administrator@PC027>
2019-04-02 16:14:20 +0700

    change tiep ne

--- BigSchool/ViewModel/CourseViewModel.cs
+++ BigSchool/ViewModel/CourseViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,9 +10,14 @@ namespace BigSchool.ViewModel
 {
     public class CourseViewModel
     {
-        public string Place { get; set; }
+        [Required]
+        public string Place { get; set;}
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+        [Required]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime()
